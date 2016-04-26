3170dc7f833ef75382ac043193f3216ff692cc18
ochicf <oscarchicfarre@gmail.com>
2016-04-26 18:30:17 +0200

    quickfields and omitFields
    by default all fields in the astrClass will be rendered unless there are some children inside the Form component. Fields can be omitted with omitField prop

--- client/components/form.jsx
+++ client/components/form.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import _ from 'lodash';
 
+import Field from '../containers/field.js';
 import formContextTypes from '../utils/form_context_types.js';
 
 class Form extends React.Component {
+  /**
+   * Set context for form's children.
+   *
+   * @return {object} Child context
+   * @override
+   */
   getChildContext() {
     const { doc, astroClass } = this.props;
     return {
@@ -10,10 +18,45 @@ class Form extends React.Component {
       astroClass: astroClass,
     };
   }
+  /**
+   * Return an array with the names of the fields to omit.
+   *
+   * @return {Array} Fields to omit
+   */
+  getFieldsToOmit() {
+    let fieldsToOmit = this.props.omitFields;
+    if (_.isString(fieldsToOmit)) {
+      fieldsToOmit = fieldsToOmit.split(',');
+    }
+    return _.isArray(fieldsToOmit) ? fieldsToOmit : [];
+  }
+  /**
+   * Get children components for this form. In case no children are specified,
+   * return all fields for this component's ```astroClass``` but those listed in the
+   * ```omitFields``` property.
+   *
+   * @return {Object|Array} Component children
+   */
+  getChildren() {
+    if (this.props.children) {
+      // custom fields, show childs
+      return this.props.children;
+    } else {
+      // no children specified, show all fields
+      const fieldExceptions = this.getFieldExceptions();
+      return _.map(this.props.astroClass.getInputsNames(), (fieldName) => {
+        if (_.indexOf(fieldExceptions, fieldName) < 0) {
+          return (
+            <Field key={fieldName} name={fieldName} />
+          );
+        }
+      });
+    }
+  }
   render() {
     return (
       <form>
-        {this.props.children}
+        {this.getChildren()}
       </form>
     );
   }
