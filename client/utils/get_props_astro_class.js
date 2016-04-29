1efb9401e6d7b89db6f41e3b17fabd1c96e23bab
ochicf <oscarchicfarre@gmail.com>
2016-04-29 13:49:57 +0200

    added documentation

--- client/utils/get_props_astro_class.js
+++ client/utils/get_props_astro_class.js
@@ -1,5 +1,14 @@
 import { Class } from 'meteor/jagi:astronomy';
 
+/**
+ * Retrieves astro class from recieved object by first trying to do it from
+ * the ```doc``` property, and then from ```astroClass``` property.
+ * 
+ * @param  {object} props
+ *  Props object. Expected properties: ```doc``` and ```astroClass```.
+ * @return {Astro.Class|undefined}
+ *  Astronomy class, undefined if could not retrieve it or invalid.
+ */
 function getPropsAstroClass(props) {
   let astroClass;
   if (_.isObject(props.doc) && (props.doc instanceof Class)) {
